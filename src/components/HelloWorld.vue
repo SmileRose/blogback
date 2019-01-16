65952ff21f48408150729488e7ca462d42c9781b
Rose <hey_cool@163.com>
2019-01-16 17:59:28 +0800

    添加vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,10 +2,7 @@
   <div class="hello">
     <el-container>
       <el-aside width="200px">
-       <!--  <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
-          <el-radio-button :label="false">展开</el-radio-button>
-          <el-radio-button :label="true">收起</el-radio-button>
-        </el-radio-group> -->
+
         <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
           <el-submenu index="1">
             <template slot="title">
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import home from '@/components/home'
+import addArticle from '@/components/addArticle'
+import addMenu from '@/components/addMenu'
+import listArticle from '@/components/listArticle'
 
 Vue.use(Router)
 
@@ -8,8 +12,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: home
     }
   ]
 })
