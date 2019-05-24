6e6b7361fd880227646f82a2039aa610db6a9f59
Rose <hey_cool@163.com>
2019-05-24 18:23:48 +0800

    开始

---
+++ src/components/menu.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="manage_page fillcontain">
+		<el-row style="height: 100%;">
+			<el-col :span="4"  style="min-height: 100%; background-color: #324057;">
+				<el-menu :default-active="defaultActive" style="min-height: 100%;" theme="dark" router   background-color="#324057"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+					<el-menu-item index="home"><i class="el-icon-menu"></i>首页</el-menu-item>
+					<el-submenu index="2">
+						<template slot="title"><i class="el-icon-document"></i>内容管理</template>
+						<el-menu-item index="articlelist">文章列表</el-menu-item>
+						<el-menu-item index="articleadd">添加文章</el-menu-item>
+					</el-submenu>
+					<el-submenu index="3">
+						<template slot="title"><i class="el-icon-star-on"></i>模块栏目</template>
+						<el-menu-item index="memuset">栏目设置</el-menu-item>
+						<el-menu-item index="menuadd">添加栏目</el-menu-item>
+					</el-submenu>
+					<el-submenu index="4">
+						<template slot="title"><i class="el-icon-plus"></i>设置</template>
+						<el-menu-item index="website">站点管理</el-menu-item>
+						<el-menu-item index="webcookieclear">缓存管理</el-menu-item>
+						<el-menu-item index="webpwd">修改密码</el-menu-item>
+					</el-submenu>
+					<!-- <el-submenu index="5">
+						<template slot="title"><i class="el-icon-setting"></i>站点管理</template>
+						<el-menu-item index="webset">站点信息</el-menu-item>
+						<el-menu-item index="seo">SEO</el-menu-item>
+					</el-submenu> -->
+					<el-submenu index="6">
+						<template slot="title"><i class="el-icon-warning"></i>关于</template>
+						<el-menu-item index="about">关于</el-menu-item>
+					</el-submenu>
+				</el-menu>
+			</el-col>
+			<el-col :span="20" style="height: 100%;overflow: auto;">
+				<keep-alive>
+					<router-view></router-view>
+				</keep-alive>
+			</el-col>
+		</el-row>
+	</div>
+</template>
+<script>
+	export default {
+		computed: {
+			defaultActive: function(){
+				return this.$route.path.replace('/', '');
+			}
+		},
+	}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,69 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+//import HelloWorld from '@/components/HelloWorld'
+Vue.use(Router)
+const menu = r => require.ensure([], () => r(require('@/components/menu')), 'menu');
+const login = r => require.ensure([], () => r(require('@/pages/login')), 'login');
+const home = r => require.ensure([], () => r(require('@/pages/home')), 'home');
+const articlelist = r => require.ensure([], () => r(require('@/pages/articlelist')), 'articlelist');
+const articleadd = r => require.ensure([], () => r(require('@/pages/articleadd')), 'articleadd');
+const memuset = r => require.ensure([], () => r(require('@/pages/memuset')), 'memuset');
+const menuadd = r => require.ensure([], () => r(require('@/pages/menuadd')), 'menuadd');
+const website = r => require.ensure([], () => r(require('@/pages/website')), 'website');
+const webcookieclear = r => require.ensure([], () => r(require('@/pages/webcookieclear')), 'webcookieclear');
+const webpwd = r => require.ensure([], () => r(require('@/pages/webpwd')), 'webpwd');
+const about = r => require.ensure([], () => r(require('@/pages/about')), 'about');
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: login
+    },{
+        path: '/menu',
+        component: menu,
+        name: '',
+        children: [{
+            path: '/home',
+            component: home,
+            meta: [],
+        }, {
+            path: '/articlelist',
+            component: articlelist,
+            meta: ['内容管理', '文章列表'],
+        },{
+            path: '/articleadd',
+            component: articleadd,
+            meta: ['内容管理', '添加文章'],
+        },
+        {
+            path: '/memuset',
+            component: memuset,
+            meta: ['模块栏目', '栏目设置'],
+        },{
+            path: '/menuadd',
+            component: menuadd,
+            meta: ['模块栏目', '添加栏目'],
+        },
+        {
+            path: '/website',
+            component: website,
+            meta: ['设置', '站点管理'],
+        },{
+            path: '/webcookieclear',
+            component: webcookieclear,
+            meta: ['设置', '缓存管理'],
+        },
+        {
+            path: '/webpwd',
+            component: webpwd,
+            meta: ['设置', '修改密码'],
+        },
+        {
+            path: '/about',
+            component: about,
+            meta: ['关于', '关于'],
+        }
+        ]
+    }]
+})
